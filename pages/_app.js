c61274f0b139e3a60ade6c71f56781c63aba6906
Evan OSMONT <e.osmont@pickup-services.com>
2022-06-13 10:56:04 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/index.scss";
+import Head from "next/head";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>OSMONT Evan</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <ParallaxProvider>
+        <Component {...pageProps} />
+      </ParallaxProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
